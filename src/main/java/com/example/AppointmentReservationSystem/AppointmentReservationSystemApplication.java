1dd39ac455bd5e5a2cdcb0ffcf5bbb92391d2365
Habiba-Sultan-Rega <hrega@miu.edu>
2021-09-19 20:23:01 -0500

    initial commit

---
+++ src/main/java/com/example/AppointmentReservationSystem/AppointmentReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.AppointmentReservationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentReservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AppointmentReservationSystem/AppointmentReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AppointmentReservationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
